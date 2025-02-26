18f9e48ce0f5ade36a10bfa89f6fcda0d9aa6662
Mason <masoncfrancis@gmail.com>
2025-02-26 15:06:06 -0500

    Added some more test images, and added a note

--- testyolo.py
+++ testyolo.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("runs/detect/train9/weights/best.pt")  # my trained model
 
 # Run batched inference on a list of images
-results = model(["testimage1.jpg", "testimage2.jpg"])  # return a list of Results objects
+results = model(["testimage1.jpg", "testimage2.jpg", "testimage3.jpg", "testimage4.jpg"])  # return a list of Results objects
 
 # Process results list
 for result in results:
@@ -14,3 +14,4 @@ for result in results:
     probs = result.probs  # Probs object for classification outputs
     obb = result.obb  # Oriented boxes object for OBB outputs
     result.show()  # display to screen
+    # I can get the number of boxes in an image using len(result.boxes)
